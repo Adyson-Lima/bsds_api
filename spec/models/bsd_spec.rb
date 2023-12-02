2abb43b6215d5e5ba56a98375e3cca5714c0672f
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-12-02 14:51:57 -0300

    2° commit - tdd model: name consegue ser preenchido? RED

---
+++ spec/models/bsd_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Bsd, type: :model do
+
+  before{@bsd = Bsd.new}
+
+  describe 'Testes de preenchimento do model Bsd' do
+
+    it 'name consegue ser preenchido?' do
+      @bsd.name = ''
+      expect(@bsd.name).to eq('openbsd')
+    end
+
+  end
+
+end
